488acf2792ee27c0edf4608d5025809f458b2efe
Ravi Sharma <ravisharma82106@gmail.com>
2023-12-28 18:19:30 +0530

    set explicit provider version

---
+++ explicit_version/explicot_version.tf
@@ -0,0 +1,25 @@
+terraform {
+
+required_version = "1.6.6"
+required_providers {
+aws= {
+source = "hashicorp/aws"
+version = "~> 5.31.0"
+}
+}
+}
+
+provider "aws" {
+region = "us-east-1"
+}
+
+resource "aws_iam_user" "lb" {
+name = "lb"
+path = "/system"
+
+
+tags = {
+name = "dev"
+
+}
+}
